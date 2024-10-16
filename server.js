dfee36886a2e6a02750a610ddb3c5d4c5f71c6cb
lip8023 <bangbangsang8023@gmail.com>
2024-10-16 18:11:58 +0800

    update: learning javascript,jquery,modal etc.

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-<<<<<<< HEAD
 var express = require("express")
 var app     = express()
+
+
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 var port    = process.env.port || 3000;
 app.listen(port,()=>{
     console.log("App listening to:" + port)
-=======
-var express = require("express")
-var app     = express()
-var port    = process.env.port || 3000;
-app.listen(port,()=>{
-    console.log("App listening to:" + port)
->>>>>>> 57f7a29b48fbe83d66bdae37509d1e6031d96d8c
 })
