d7d7e16b67f1082f86014cfef6343ec30704ce55
lip8023 <bangbangsang8023@gmail.com>
2024-10-17 16:40:10 +0800

    socket learing

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,24 +3,26 @@ const cardList = [
         title: "Kitten 1",
         image: "images/kitten-1.png",
         link: "About Kitten 1",
-        desciption: "Demo desciption about kitten 1"
+        description: "Demo description about kitten 1" // 修正拼写错误
     },
     {
         title: "Kitten 2",
         image: "images/kitten-2.png",
         link: "About Kitten 2",
-        desciption: "Demo desciption about kitten 2"
+        description: "Demo description about kitten 2" // 修正拼写错误
     },
     {
         title: "Kitten 3",
         image: "images/kitten-3.png",
         link: "About Kitten 3",
-        desciption: "Demo desciption about kitten 3"
+        description: "Demo description about kitten 3" // 修正拼写错误
     }
-]
+];
+
 const clickMe = () => {
     $('#modal1').modal('open'); // 打开模态框
-}
+};
+
 const submitForm = () => {
     let formData = {};
     formData.first_name = $('#first_name').val();
@@ -28,34 +30,37 @@ const submitForm = () => {
     formData.password = $('#password').val();
     formData.email = $('#email').val();
     console.log("Form Data Submitted: ", formData);
-}
+};
+
 const addCards = (items) => {
     items.forEach(item => {
-    let itemToAppend = '<div class="col s4 center-align">'+
-    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
-    '</div><div class="card-content">'+
-    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
-    '<div class="card-reveal">'+
-    '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-    '<p class="card-text">'+item.desciption+'</p>'+
-    '</div></div></div>';
-    $("#card-section").append(itemToAppend)
+        let itemToAppend = '<div class="col s4 center-align">' +
+            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '">' +
+            '</div><div class="card-content">' +
+            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
+            '<div class="card-reveal">' +
+            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
+            '<p class="card-text">' + item.description + '</p>' + // 修正拼写错误
+            '</div></div></div>';
+        $("#card-section").append(itemToAppend);
     });
-}
-$(document).ready(function(){
+};
+
+$(document).ready(function() {
     $('.materialboxed').materialbox();
-    $('#clickMeButton').click(clickMe); // 修改这里
-    $('#formSubmit').click(()=>{
-    submitForm();})
-    //addCards(cardList);
+    $('#clickMeButton').click(clickMe);
+    $('#formSubmit').click(() => {
+        submitForm();
+    });
+    // addCards(cardList); // 选择保留或注释
     getProjects();
     $('.modal').modal();
 });
 
 const getProjects = () => {
-    $.get('/api/projects',(response) => {
-    if(response.statusCode==200){
-        addCards(response.data);
-    }
-    })
-}
+    $.get('/api/projects', (response) => {
+        if (response.statusCode == 200) {
+            addCards(response.data);
+        }
+    });
+};
--- server.js
+++ server.js
@@ -1,32 +1,51 @@
-var express = require("express")
-var app = express()
-app.use(express.static(__dirname+'/public'))
+var http = require("http");
+var express = require("express");
+var app = express();
+var server = http.createServer(app); // 创建 HTTP 服务器
+
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 const cardList = [
     {
         title: "Kitten 1",
         image: "images/kitten-1.png",
         link: "About Kitten 1",
-        desciption: "Demo desciption about kitten 1"
+        description: "Demo description about kitten 1" // 修正拼写错误
     },
     {
         title: "Kitten 2",
         image: "images/kitten-2.png",
         link: "About Kitten 2",
-        desciption: "Demo desciption about kitten 2"
+        description: "Demo description about kitten 2" // 修正拼写错误
     },
     {
         title: "Kitten 3",
         image: "images/kitten-3.png",
         link: "About Kitten 3",
-        desciption: "Demo desciption about kitten 3"
+        description: "Demo description about kitten 3" // 修正拼写错误
     }
-]
-app.get('/api/projects',(req,res) => {
-    res.json({statusCode: 200, data: cardList, message:"Success"})
-})
-var port = process.env.port || 3000;
-    app.listen(port,()=>{
-    console.log("App listening to: "+port)
-})
+];
+
+app.get('/api/projects', (req, res) => {
+    res.json({ statusCode: 200, data: cardList, message: "Success" });
+});
+
+var port = process.env.PORT || 3000; // 修正为 PORT
+server.listen(port, () => { // 使用 server.listen
+    console.log("App listening to: " + port);
+});
+
+let io = require('socket.io')(server); // 将 server 传递给 Socket.IO
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    setInterval(() => {
+        socket.emit('number', parseInt(Math.random() * 10));
+    }, 1000);
+});
